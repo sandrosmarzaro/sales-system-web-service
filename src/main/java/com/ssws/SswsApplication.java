d4b81f9523a07fc8ea4cfe1dbb1e9bcb86ded8d1
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-06-28 21:20:37 -0300

    [ADDED] first commit

---
+++ src/main/java/com/ssws/SswsApplication.java
@@ -0,0 +1,13 @@
+package com.ssws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SswsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SswsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssws/SswsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SswsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
