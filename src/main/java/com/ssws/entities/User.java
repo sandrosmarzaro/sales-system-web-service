07d11460d2f85978692fa80f9cd2a4e5bafb96e7
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-07-04 10:09:29 -0300

    [ADDED] jpa and h2 database with test profile

--- src/main/java/com/ssws/entities/User.java
+++ src/main/java/com/ssws/entities/User.java
@@ -2,19 +2,48 @@ package com.ssws.entities;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
+@Entity
+@Table(name="`user`")
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 public class User implements Serializable {
 
+    @Id
+    @Column(name="`id`")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
+    @Column(name="`name`")
     private String name;
+    @Column(name="`email`")
     private String email;
+    @Column(name="`phone`")
     private String phone;
+    @Column(name="`password`")
     private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id != null ? id.equals(user.id) : user.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
